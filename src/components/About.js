f3707d260672e73339f9d4809d21d6b13ca315ae
fihra <fabrofabian@gmail.com>
2023-02-10 22:10:17 -1000

    made style adjustments to container, text colors, and moved image

--- src/components/About.js
+++ src/components/About.js
@@ -14,10 +14,24 @@ const About = (props) =>  {
     return(
         <div className="about-container">
             <main>
-                <h1 style={{textAlign: "left", marginLeft: 180}}>About</h1>
+                <h1 className="about-header">About</h1>
+                <button onClick={handleClick} className="back-button" style={{position: 'absolute', top: 25} }><ArrowForwardIcon/></button>
+                <div className="about-paragraph">
                 <img className="solo-headshot" src={myImg} alt="solo-headshot"/>
-                <button onClick={handleClick} className="back-button" style={{position: 'absolute', top: 30} }><ArrowForwardIcon/></button>
-                <p className="about-paragraph">Hi! I'm Fabian. I am a technical audio designer, software developer, composer, and musician. From integrating music & audio into games, creating music and sfx, to building web & software apps. I'm passionate with combining my skills of audio & technology. I have worked on collaborative game jams as a technical sound designer & composer. I've also developed my own solo game projects with programming, music, sound design, & art. Built a variety of software apps, from web apps to an audio plugin. </p>
+                <p >Hi! I'm <span>Fabian</span>. I am a <span>technical audio designer</span>, <span>software developer</span>, <span>composer</span>, and <span>musician</span>. From integrating music & audio into games, creating music and sfx, to building web & software apps. </p>
+                <p>
+                I'm passionate with combining my skills of audio & technology. I have worked on collaborative game jams as a technical sound designer, audio implementer & composer. I've also developed my own solo game projects with programming, music, sound design, & art. Built a variety of software apps, from web apps to an audio plugin. 
+                </p>
+                {/* <h2>Experience</h2>
+                <p>Software Developer @ <b>Servco Pacific Inc.</b>, March 2022 - Present</p>
+                <ul>
+                    <li>Update & maintain </li>
+                    <li>Task 2</li>
+                </ul>
+                <h2>Education</h2>
+                <p><b>Flatiron School</b>, August 2019 | <i>Seattle, WA</i> 15-week intensive coding bootcamp in Software Engineering</p>
+                <p><b>Unviversity of Hawai'i at Manoa</b>, December 2016 | <i>Honolulu, HI</i> Bachelor of Arts: Music & Anthropology</p> */}
+                </div>
             </main>
             <SkillsData/>
             
